d9bb533c5e078cb6e185f0cd5640ff10213c816e
Md Fahim Hossain <alifhossain174@gmail.com>
2023-12-16 00:50:18 +0600

    OS Detection Bug Fixed

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -420,6 +420,46 @@ class FrontendController extends Controller
         );
     }
 
+    public function getOS() {
+
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+        $os_platform =   "Bilinmeyen İşletim Sistemi";
+        $os_array =   array(
+            '/windows nt 10/i'      =>  'Windows 10',
+            '/windows nt 6.3/i'     =>  'Windows 8.1',
+            '/windows nt 6.2/i'     =>  'Windows 8',
+            '/windows nt 6.1/i'     =>  'Windows 7',
+            '/windows nt 6.0/i'     =>  'Windows Vista',
+            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+            '/windows nt 5.1/i'     =>  'Windows XP',
+            '/windows xp/i'         =>  'Windows XP',
+            '/windows nt 5.0/i'     =>  'Windows 2000',
+            '/windows me/i'         =>  'Windows ME',
+            '/win98/i'              =>  'Windows 98',
+            '/win95/i'              =>  'Windows 95',
+            '/win16/i'              =>  'Windows 3.11',
+            '/macintosh|mac os x/i' =>  'Mac OS X',
+            '/mac_powerpc/i'        =>  'Mac OS 9',
+            '/linux/i'              =>  'Linux',
+            '/ubuntu/i'             =>  'Ubuntu',
+            '/iphone/i'             =>  'iPhone',
+            '/ipod/i'               =>  'iPod',
+            '/ipad/i'               =>  'iPad',
+            '/android/i'            =>  'Android',
+            '/blackberry/i'         =>  'BlackBerry',
+            '/webos/i'              =>  'Mobile'
+        );
+
+        foreach ( $os_array as $regex => $value ) {
+            if ( preg_match($regex, $user_agent ) ) {
+                $os_platform = $value;
+            }
+        }
+        return $os_platform;
+    }
+
+
     public function biodataDetails(Request $request, $slug){
 
         $biodata_details_url = $request->path();
@@ -448,7 +488,7 @@ class FrontendController extends Controller
             'user_id' => Auth::user() ? Auth::user()->id : null,
             'from_ip_address' => $this->get_client_ip(),
             'browser' => $yourbrowser,
-            'os' => PHP_OS,
+            'os' => $this->getOS(),
             'device' => $isMob ? 'Mobile' : 'Desktop',
             'created_at' => Carbon::now()
         ]);
